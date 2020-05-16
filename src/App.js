423ea1cb7d932a26ce3e8f68789a0c6e03bc89c5
Tim Scott <tscott@lunaversesoftware.com>
2020-05-16 17:47:26 -0500

    Some stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* global fetch */
 import React, {useState, useEffect} from 'react';
 import styled, {createGlobalStyle} from 'styled-components'
-import {LineChart, Line, YAxis, XAxis, Label} from 'recharts';
+import {LineChart, Line, YAxis, XAxis, Label, ReferenceLine} from 'recharts';
 import {DateTime} from 'luxon';
 
 const GlobalStyle = createGlobalStyle`
@@ -34,7 +34,7 @@ const App = () => {
       });
       setData(historical);
     });
-  });
+  }, []);
 
   if (!data) {
     return 'Loading...'
@@ -46,12 +46,25 @@ const App = () => {
       <div>
         <h2>ICU Beds Used - Georgia</h2>
         <LineChart
-          width={600}
-          height={600}
+          width={1000}
+          height={1000}
           data={data}
           margin={{top: 20, right: 20, bottom: 100, left: 20}}
         >
+          <ReferenceLine
+            x="2020-04-24"
+            stroke="#aaa"
+            strokeWidth={2}
+            label={({viewBox: {x, y, width, height}, offset}) => {
+              return (
+                <text x={x} y={y + height/2} style={{writingMode: 'tb'}} dx={20} >
+                  reopening begins
+                </text>
+              );
+            }}
+          />
           <Line
+            dot={false}
             type="monotone"
             dataKey="ICUBedsInUse"
             stroke="#8884d8"
