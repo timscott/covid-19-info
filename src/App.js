aa233f385fe50575ff01684f4789f56e71a25539
Tim Scott <tscott@lunaversesoftware.com>
2020-05-16 16:36:53 -0500

    Added a chart!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 /* global fetch */
 import React, {useState, useEffect} from 'react';
 import styled, {createGlobalStyle} from 'styled-components'
+import {LineChart, Line, YAxis, XAxis, Label} from 'recharts';
+import {DateTime} from 'luxon';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -18,26 +20,53 @@ const Container = styled.div`
 `;
 
 const App = () => {
-  const [message, setMessage] = useState('...loading')
+  const [data, setData] = useState()
 
   useEffect(() => {
-    async function fetchData () {
-      try {
-        let data = await (await fetch('/api')).json()
-        setMessage(data.message)
-      } catch (err) {
-        setMessage(err.message)
-      }
-    }
-    fetchData()
-  })
+    fetch('https://data.covidactnow.org/snapshot/296/us/states/GA.NO_INTERVENTION.timeseries.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const historical = data.timeseries
+      .filter(({date}) => {
+        return DateTime.fromISO(date) <= DateTime.local();
+      });
+      setData(historical);
+    });
+  });
+
+  if (!data) {
+    return 'Loading...'
+  }
 
   return (
     <Container>
       <GlobalStyle />
-      <header>
-        <p>{message}</p>
-      </header>
+      <div>
+        <h2>ICU Beds Used - Georgia</h2>
+        <LineChart
+          width={600}
+          height={600}
+          data={data}
+          margin={{top: 20, right: 20, bottom: 100, left: 20}}
+        >
+          <Line
+            type="monotone"
+            dataKey="ICUBedsInUse"
+            stroke="#8884d8"
+            strokeWidth={4}
+          />
+          <YAxis domain={[0, 300]} />
+          <XAxis
+            dataKey="date"
+            tickMargin="10"
+            tickFormatter={value => DateTime.fromISO(value).toFormat('M/d')}
+          >
+            <Label value="Date" position="bottom" offset={20} />
+          </XAxis>
+        </LineChart>
+      </div>
     </Container>
   );
 }
