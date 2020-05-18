a954b54da660da56cff6b8856eef8fbc2378745b
Tim Scott <tscott@lunaversesoftware.com>
2020-05-18 18:53:57 -0500

    Nicer menu.

--- src/Menu.js
+++ src/Menu.js
@@ -4,11 +4,28 @@ import styled from 'styled-components'
 import {Link, useLocation} from 'react-router-dom';
 import states from './states';
 
-const StateLinksContainer = styled.div``;
+const StateLinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`;
 
 const StateLink = styled(Link)`
-  margin-right: 20px;
+  text-decoration: none;
+  color: #333;
+  display: flex;
+  margin: 0 5px 5px;
+  padding: 6px 8px;
+  border: solid 1px #aaa;
+  background-color: #ddd;
+  border-radius: 4px;
+   transition: 0.4s;
+  &:hover {
+    background-color: #bbb;
+  }
 `;
+
 const Menu = () => {
   const query = new URLSearchParams(useLocation().search);
   return (
