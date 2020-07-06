c160b1b6cea2b77a66a2019de70103923092f6bb
Tim Scott <tscott@lunaversesoftware.com>
2020-07-06 14:12:59 -0500

    Front end env vars.

--- src/Locale.js
+++ src/Locale.js
@@ -7,7 +7,7 @@ import states from './states';
 import Chart from './Chart';
 
 const {
-  API_VERSION = '601'
+  REACT_APP_API_VERSION: API_VERSION = '601'
 } = process.env;
 
 const SettingsContainer = styled.div`
